1dfcb2a09b313e0885e31b1474621d163f3cb426
ahslhx <ahslhx@gmail.com>
2017-12-20 18:28:14 +0800

    update

--- ensemble.py
+++ ensemble.py
@@ -1,4 +1,5 @@
 import pickle
+import numpy as np
 
 class AdaBoostClassifier:
     '''A simple AdaBoost Classifier.'''
@@ -10,7 +11,11 @@ class AdaBoostClassifier:
             weak_classifier: The class of weak classifier, which is recommend to be sklearn.tree.DecisionTreeClassifier.
             n_weakers_limit: The maximum number of weak classifier the model can use.
         '''
-        pass
+        print('Initialize AdaBoostClassifier')
+        self.weak_classifier = weak_classifier
+        self.n_weakers_limit = n_weakers_limit
+        self.weak_classifier_list = []
+        self.alpha_list = []
 
     def is_good_enough(self):
         '''Optional'''
@@ -23,7 +28,26 @@ class AdaBoostClassifier:
             X: An ndarray indicating the samples to be trained, which shape should be (n_samples,n_features).
             y: An ndarray indicating the ground-truth labels correspond to X, which shape should be (n_samples,1).
         '''
-        pass
+        n_samples, n_features = X.shape
+        distribution = np.ones(n_samples) * (1 / n_samples)
+        print(distribution.shape)
+
+        for i in range(self.n_weakers_limit):
+            print('Building No.', i + 1, 'Weak Classifier...')
+            weak_classifier = self.weak_classifier
+            weak_classifier.fit(X, y, sample_weight=distribution)
+            self.weak_classifier_list.append(weak_classifier)
+            pred_y = weak_classifier.predict(X)
+            error = np.sum(distribution * (pred_y != y))
+            alpha = np.log((1.0-error)/max(error,1e-16))/2.0
+            self.alpha_list.append(alpha)
+
+            for j in range(n_samples):
+                distribution[j] = distribution[j] * np.exp(-alpha * y[j] * pred_y[j])
+            distribution = distribution / np.sum(distribution)
+
+
+        return self
 
 
     def predict_scores(self, X):
@@ -33,12 +57,20 @@ class AdaBoostClassifier:
             X: An ndarray indicating the samples to be predicted, which shape should be (n_samples,n_features).
 
         Returns:
-            An one-dimension ndarray indicating the scores of differnt samples, which shape should be (n_samples,1).
+            An one-dimension ndarray indicating the scores of different samples, which shape should be (n_samples,1).
         '''
-        pass
+        n_samples = X.shape[0]
+        n_weakers_limit = self.n_weakers_limit
+        score = np.zeros((n_samples, 1))
+        for i in range(n_weakers_limit):
+            score_i = self.weak_classifier_list[i].predict(X).reshape(n_samples, 1)
+            score = score + self.alpha_list[i] * score_i
+
+        return score
+
 
     def predict(self, X, threshold=0):
-        '''Predict the catagories for geven samples.
+        '''Predict the catagories for given samples.
 
         Args:
             X: An ndarray indicating the samples to be predicted, which shape should be (n_samples,n_features).
@@ -47,7 +79,13 @@ class AdaBoostClassifier:
         Returns:
             An ndarray consists of predicted labels, which shape should be (n_samples,1).
         '''
-        pass
+        n_samples = X.shape[0]
+        score = self.predict_scores(X)
+        pred_y = np.ones((n_samples, 1))
+        pred_y[score < threshold] = -1
+
+        return pred_y
+
 
     @staticmethod
     def save(model, filename):
--- train.py
+++ train.py
@@ -1,6 +1,62 @@
+import os
+import numpy as np
+from PIL import Image
+from feature import *
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
+from ensemble import AdaBoostClassifier
+
+def get_original_features(file):
+    features = []
+    path = "./datasets/original/"+file
+    for filename in os.listdir(path):
+        img = Image.open(path + "/" + filename).convert('L')
+        img = img.resize((24, 24))
+        img = np.array(img)
+
+        img_ndp = NPDFeature(img)
+        this_feature = img_ndp.extract()
+        features.append(this_feature)
+
+    np.save("./datasets/"+file+"_features.npy", features)
+
+def get_features_dataset(face_features,nonface_features):
+    num_face_sample, num_face_feature = face_features.shape
+    num_nonface_sample, num_nonface_feature = nonface_features.shape
+
+    positive_label = [np.ones(1) for i in range(num_face_sample)]
+    negative_label = [-np.ones(1) for i in range(num_nonface_sample)]
+
+    positive_samples = np.concatenate((face_features, positive_label), axis=1)
+    negative_samples = np.concatenate((nonface_features, negative_label), axis=1)
+
+    features_dataset = np.concatenate((positive_samples, negative_samples), axis=0)
+
+    np.random.shuffle(features_dataset)
+    np.save("./datasets/extract_features",features_dataset)
 
 
 if __name__ == "__main__":
-    # write your code here
-    pass
+    get_original_features("face")
+    get_original_features("nonface")
+    face_features = np.load("./datasets/face_features.npy")
+    nonface_features = np.load("./datasets/nonface_features.npy")
+    get_features_dataset(face_features,nonface_features)
+    features_dataset = np.load("./datasets/extract_features.npy")
+    print(features_dataset.shape,face_features.shape,nonface_features.shape)
+    num_face_feature = features_dataset.shape[1] - 1
+
+    training_size = 800
+    X_train = features_dataset[:training_size, :num_face_feature]
+    X_validation = features_dataset[training_size:, :num_face_feature]
+
+    y_train = features_dataset[:training_size, -1]
+    y_validation = features_dataset[training_size:, -1]
+    # print(X_train.shape,y_train.shape,X_validation.shape,y_validation.shape)
+    adaboost_classifier = AdaBoostClassifier(DecisionTreeClassifier(max_depth=4),5)
+    pred_y = adaboost_classifier.fit(X_train, y_train).predict(X_validation)
+
+    with open("report.txt", "wb") as f:
+        report = classification_report(y_validation, pred_y, target_names = ["nonface","face"])
+        f.write(report.encode())
 
